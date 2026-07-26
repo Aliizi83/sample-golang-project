c870cb247a8312bd3e14e9d669695cd59789e6d9
Aliizi <aliizi1383@gmail.com>
2026-07-26 23:50:20 +0330

    connection to redis completed

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"github.com/Aliizi83/sample-golang-project/src/api"
+	"github.com/Aliizi83/sample-golang-project/src/config"
+	"github.com/Aliizi83/sample-golang-project/src/infra/cache"
 )
 
 func main() {
-	api.InitServer()
+
+	cfg := config.GetConfig()
+	cache.InitRedis(cfg)
+	defer cache.CloseRedis()
+
+	api.InitServer(cfg)
 }
---
+++ src/infra/cache/redis.go
@@ -0,0 +1,31 @@
+package cache
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/Aliizi83/sample-golang-project/src/config"
+	"github.com/redis/go-redis/v9"
+)
+
+var redisClient *redis.Client
+
+func InitRedis(cfg *config.Config) {
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:         fmt.Sprintf("%s:%s", cfg.Redis.Host, cfg.Redis.Port),
+		Password:     cfg.Redis.Password,
+		DialTimeout:  cfg.Redis.DialTimeout * time.Second,
+		ReadTimeout:  cfg.Redis.ReadTimeout * time.Second,
+		WriteTimeout: cfg.Redis.WriteTimeout * time.Second,
+		PoolSize:     cfg.Redis.PoolSize,
+		PoolTimeout:  cfg.Redis.PoolTimeout * time.Second,
+	})
+}
+
+func GetRedis() *redis.Client {
+	return redisClient
+}
+
+func CloseRedis() {
+	redisClient.Close()
+}
