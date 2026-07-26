61736d6d36e960178f3e07ec553651e4a02f7183
Aliizi <aliizi1383@gmail.com>
2026-07-26 21:23:14 +0330

    common string utilities

--- src/api/api.go
+++ src/api/api.go
@@ -15,10 +15,20 @@ func InitServer() {
 	r := gin.New()
 	r.Use(gin.Logger(), gin.Recovery())
 
-	v1 := r.Group("/api/v1/")
+	api := r.Group("/api")
 	{
-		health := v1.Group("/health")
-		routers.Health(health)
+		v1 := api.Group("/v1")
+		{
+			health := v1.Group("/health")
+			routers.Health(health)
+		}
+
+		v2 := api.Group("/v2")
+		{
+			health := v2.Group("/health")
+			routers.Health(health)
+
+		}
 	}
 
 	r.Run(fmt.Sprintf(":%s", cfg.Server.InternalPort))
