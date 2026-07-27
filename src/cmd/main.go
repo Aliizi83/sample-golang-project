071727345df9a30ebe96c7b9b7a91ff641e8f557
Aliizi <aliizi1383@gmail.com>
2026-07-27 13:25:08 +0330

    connection to postgres stablished

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Aliizi83/sample-golang-project/src/api"
 	"github.com/Aliizi83/sample-golang-project/src/config"
 	"github.com/Aliizi83/sample-golang-project/src/infra/cache"
+	"github.com/Aliizi83/sample-golang-project/src/infra/db"
 )
 
 func main() {
@@ -17,6 +18,11 @@ func main() {
 		log.Fatal("Redis connection failed: " + err.Error())
 		return
 	}
+
+	if err := db.InitDB(cfg); err != nil {
+		log.Fatal("Postgres conection failed: " + err.Error())
+	}
+	defer db.CloseDB()
 
 	api.InitServer(cfg)
 }
---
+++ src/infra/db/postgres.go
@@ -0,0 +1,51 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Aliizi83/sample-golang-project/src/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var dbClient *gorm.DB
+
+func InitDB(cfg *config.Config) error {
+	cnn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s TimeZone=Asia/Tehran",
+		cfg.Postgres.Host, cfg.Postgres.Port, cfg.Postgres.User, cfg.Postgres.Password, cfg.Postgres.DbName, cfg.Postgres.SSLMode)
+
+	dbClient, err := gorm.Open(postgres.Open(cnn), &gorm.Config{})
+
+	if err != nil {
+		return err
+	}
+
+	sqlDb, err := dbClient.DB()
+	if err != nil {
+		return err
+	}
+
+	err = sqlDb.Ping()
+	if err != nil {
+		return err
+	}
+
+	sqlDb.SetMaxIdleConns(cfg.Postgres.MaxIdleConns)
+	sqlDb.SetMaxOpenConns(cfg.Postgres.MaxOpenConns)
+	sqlDb.SetConnMaxLifetime(cfg.Postgres.ConnMaxLifetime * time.Minute)
+
+	log.Println("Db connection stablished")
+
+	return nil
+}
+
+func GetDB() *gorm.DB {
+	return dbClient
+}
+
+func CloseDB() {
+	db, _ := dbClient.DB()
+	db.Close()
+}
