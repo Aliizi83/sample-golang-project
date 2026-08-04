f3c655ed160e6021d1c8ae51130026d2bb7e5c69
Aliizi <aliizi1383@gmail.com>
2026-08-04 11:18:36 +0330

    added token field for jwt token

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -9,6 +9,9 @@ import (
 	"github.com/Aliizi83/sample-golang-project/src/pkg/logging"
 )
 
+// @securityDefinitions.apikey AuthBearer
+// @in header
+// @name Authorization
 func main() {
 
 	cfg := config.GetConfig()
